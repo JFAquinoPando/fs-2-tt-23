de3aa2fc4cfc8e261bb58f5500d14913f12e6080
JFAquinoPando <pandolcik@gmail.com>
2025-10-16 19:50:52 -0300

    Ahora convertimos a JS Moderno

--- js/ejecutar.js
+++ js/ejecutar.js
@@ -1,4 +1,11 @@
-console.log((b+a) * (b*a)  );
+import { a } from "./miscript.js";
+console.log(a);
 
+
+
+/* console.log((b+a) * (b*a)  );
+
+console.log(globalThis);
 var tituloCuerpo = document.getElementsByTagName("h1")
 tituloCuerpo[0].innerText = "Chau"
+ */
--- js/miscript.js
+++ js/miscript.js
@@ -1,1 +1,4 @@
-const a = 5
+export const a = 5
+
+const titulo = document.querySelector("h1")
+titulo.textContent = "Chau v2.0"
