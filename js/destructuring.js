48fb372a678b27527156409fa7d6703deccdc186
JFAquinoPando <pandolcik@gmail.com>
2025-10-16 20:41:28 -0300

    Se explica el destructuring

---
+++ js/destructuring.js
@@ -0,0 +1,17 @@
+/* T */
+/* Estructurar objeto u array (listado) */
+const persona = {
+    nombre: "Fabricio",
+    edad: 79
+}
+
+const {nombre} = persona
+console.log(nombre, persona.nombre ,persona.edad, edad);
+
+/* Y en listado??? */
+
+const frutas = ["🍑", "🍎", "🍏", "🍌", "🍐"]
+const [durazno, banana] = frutas
+
+console.log(frutas, durazno, banana);
+
